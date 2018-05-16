c14075190711addadb92af7e7ae16bbfc473c767
eomtaewook <trim0917@gmail.com>
2018-05-17 00:25:20 +0900

    암호화, 제너릭 싱글톤 추가

---
+++ API.Util/Crypto/DesCrypto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Util.Crypto
+{
+    public class DesCrypto
+    {
+        private static byte[] _key = Encoding.ASCII.GetBytes("Diademata");
+        public static string Encryption(string plainText)
+        {
+            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider();
+            _crypto.IV = _key;
+            _crypto.Key = _key;
+            string encryptedText = "";
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var cryStream = new CryptoStream(ms, _crypto.CreateEncryptor(), CryptoStreamMode.Write);
+                    byte[] bytes = Encoding.Unicode.GetBytes(plainText.ToCharArray());
+                    cryStream.Write(bytes, 0, bytes.Length);
+                    cryStream.FlushFinalBlock();
+                    encryptedText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return encryptedText;
+        }
+        public static string Decryption(string encryptedText)
+        {
+            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider();
+            _crypto.IV = _key;
+            _crypto.Key = _key;
+            string decryptedText = "";
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var cryStream = new CryptoStream(ms, _crypto.CreateDecryptor(), CryptoStreamMode.Write);
+                    byte[] bytes = Encoding.Unicode.GetBytes(decryptedText);
+                    cryStream.Write(bytes, 0, bytes.Length);
+                    cryStream.FlushFinalBlock();
+                    decryptedText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return decryptedText;
+        }
+    }
+}
---
+++ API.Util/DesignPattern/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Util.DesignPattern
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private readonly static object _mutex = new object();
+        private static T _instace;
+        public static T Instance()
+        {
+            try
+            {
+                Monitor.Enter(_mutex);
+                if (_instace == null)
+                {
+                    _instace = new T();
+                }
+            }
+            finally
+            {
+                Monitor.Exit(_mutex);
+            }
+            return _instace;
+        }
+    }
+}
