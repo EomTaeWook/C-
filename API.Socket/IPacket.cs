835de02e27e7d3cb5ea0614c4fb2ce30215f1620
eomtaewook <trim0917@gmail.com>
2018-07-15 23:06:52 +0900

    수정

--- API.Socket/IPacket.cs
+++ API.Socket/IPacket.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace API.Socket.InternalStructure
+namespace API.Socket
 {
     public interface IPacket : IDisposable
     {
