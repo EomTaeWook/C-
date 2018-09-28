18563b07a95e7955ad476f1e569df070691dede9
eomtaewook <trim0917@gmail.com>
2018-09-29 02:35:13 +0900

    .

--- API.Util/Crypto/DesCrypto.cs
+++ API.Util/Crypto/DesCrypto.cs
@@ -13,9 +13,11 @@ namespace API.Util.Crypto
         private static byte[] _key = Encoding.ASCII.GetBytes("Diademata");
         public static string Encryption(string plainText)
         {
-            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider();
-            _crypto.IV = _key;
-            _crypto.Key = _key;
+            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider
+            {
+                IV = _key,
+                Key = _key
+            };
             string encryptedText = "";
             try
             {
@@ -36,9 +38,11 @@ namespace API.Util.Crypto
         }
         public static string Decryption(string encryptedText)
         {
-            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider();
-            _crypto.IV = _key;
-            _crypto.Key = _key;
+            DESCryptoServiceProvider _crypto = new DESCryptoServiceProvider
+            {
+                IV = _key,
+                Key = _key
+            };
             string decryptedText = "";
             try
             {
